234d1b92bf6686fbda6daaeb9becb4fd6e25cfda
Thomas Threadgold <tj.threadgold@gmail.com>
2015-05-07 10:37:40 +1200

    Added cart functions to lib

--- moodec/lib.php
+++ moodec/lib.php
@@ -44,6 +44,68 @@ function local_moodec_extends_settings_navigation(settings_navigation $nav, $con
 }
 
 /**
+ * Formats the course summary description
+ *
+ * @param  int 		$id 	the course id
+ * @return string     		the formatted text
+ */
+function local_moodec_format_course_summary($id) {
+	global $CFG;
+	require_once $CFG->libdir . '/filelib.php';
+	require_once $CFG->libdir . '/weblib.php';
+
+	$course = get_course($id);
+
+	if (strlen($course->summary) < 1) {
+		return '';
+	}
+
+	$context = context_course::instance($course->id);
+
+	$options = array(
+		'para' => false,
+		'newlines' => true,
+		'overflowdiv' => false,
+	);
+
+	$summary = file_rewrite_pluginfile_urls($course->summary, 'pluginfile.php', $context->id, 'course', 'summary', null);
+	return format_text($summary, $course->summaryformat, $options, $course->id);
+}
+
+/**
+ * Formats the enrolment duration to
+ * @param  [type] $duration [description]
+ * @return [type]           [description]
+ */
+function local_moodec_format_enrolment_duration($duration) {
+	$output = '';
+
+	if (364 < $duration) {
+		$years = floor($duration / 365);
+		$duration = $duration % 365;
+		$output .= $years == 1 ? $years . ' year ' : $years . ' years ';
+	}
+
+	if (30 < $duration) {
+		$months = floor($duration / 30);
+		$duration = $duration % 30;
+		$output .= $months == 1 ? $months . ' month ' : $months . ' months ';
+	}
+
+	if (7 < $duration) {
+		$weeks = floor($duration / 7);
+		$duration = $duration % 7;
+		$output .= $weeks == 1 ? $weeks . ' week ' : $weeks . ' weeks ';
+	}
+
+	if (0 < $duration) {
+		$output .= $duration == 1 ? $duration . ' day ' : $duration . ' days';
+	}
+
+	return $output;
+}
+
+/**
  * Returns the url of the first image contained in the course summary file area
  * @param  int $id the course id
  * @return string     the url to the image
@@ -69,4 +131,92 @@ function local_moodec_get_course_image_url($id) {
 	}
 
 	return false;
+}
+
+/**
+ * Returns the cart from cookie
+ * @return array cart
+ */
+function local_moodec_get_cart() {
+	if (isset($_COOKIE['moodec_cart'])) {
+		return local_moodec_object_to_array(json_decode($_COOKIE['moodec_cart']));
+	}
+
+	return false;
+}
+
+/**
+ * Adds an item to the cart
+ * @param  int 	$id		course id
+ * @return array    	the cart
+ */
+function local_moodec_cart_add($id) {
+	$newCart = array();
+	$id = (int) $id;
+
+	$cart = local_moodec_get_cart();
+
+	if (!!$cart) {
+		$newCart = $cart;
+	}
+
+	$newCart['courses'][$id] = 1;
+
+	setcookie('moodec_cart', json_encode($newCart), time() + 31536000);
+
+	return $newCart;
+}
+
+function local_moodec_cart_remove($id) {
+	$newCart = array();
+	$id = (int) $id;
+
+	$cart = local_moodec_get_cart();
+
+	if (!!$cart) {
+		$newCart = $cart;
+	}
+
+	unset($newCart['courses'][$id]);
+
+	setcookie('moodec_cart', json_encode($newCart), time() + 31536000);
+
+	return $newCart;
+}
+
+/**
+ * Converts an object (and any nested objects) to an array
+ * @param  stdClass $obj 	an object or class
+ * @return array      		the converted object as an array
+ */
+function local_moodec_object_to_array($obj) {
+	if (is_object($obj)) {
+		$obj = (array) $obj;
+	}
+
+	if (is_array($obj)) {
+		$new = array();
+		foreach ($obj as $key => $val) {
+			$new[$key] = local_moodec_object_to_array($val);
+		}
+	} else {
+		$new = $obj;
+	}
+
+	return $new;
+}
+
+function local_moodec_cart_get_total() {
+	global $DB;
+	$sum = 0;
+	$cart = local_moodec_get_cart();
+
+	if (!!$cart) {
+		foreach ($cart['courses'] as $product => $value) {
+			$moodecCourse = $DB->get_record('local_moodec_course', array('courseid' => $product));
+			$sum += (float) $moodecCourse->price;
+		}
+	}
+
+	return number_format($sum, 2, '.', ',');
 }
