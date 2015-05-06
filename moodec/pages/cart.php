cf5909a47b20705adef83d524d4aec4375331b22
Thomas Threadgold <tj.threadgold@gmail.com>
2015-05-07 10:39:28 +1200

    Improved product and cart layouts/styling

--- moodec/pages/cart.php
+++ moodec/pages/cart.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Moodec Cart Page
+ *Moodec Cart Page
  *
  * @package     local
  * @subpackage  local_moodec
@@ -15,10 +15,71 @@ require_once $CFG->dirroot . '/local/moodec/lib.php';
 $PAGE->set_url('/local/moodec/pages/cart.php');
 $PAGE->set_pagelayout('standard');
 
+//
+// TODO: Check for new items and add to cart
+//
+
+// Get the cart in it's current state
+$cart = local_moodec_get_cart();
+
+// If we are adding to the cart, process this first
+if (isset($_POST['action']) && $_POST['action'] === 'addToCart') {
+	// Updates the cart var with the new addition
+	$cart = local_moodec_cart_add($_POST['id']);
+	// redirect back to the course page
+	redirect(new moodle_url('/local/moodec/pages/cart.php'));
+}
+
+if (isset($_POST['action']) && $_POST['action'] === 'removeFromCart') {
+	// Updates the cart var with the new addition
+	$cart = local_moodec_cart_remove($_POST['id']);
+	// redirect back to the course page
+	redirect(new moodle_url('/local/moodec/pages/cart.php'));
+}
+
+//
+// TODO: Render cart page
+//
 echo $OUTPUT->header();
 
-echo $OUTPUT->heading(get_string('pluginname', 'local_moodec'));
+echo $OUTPUT->heading('Cart');
 
-echo "<p>Catalogue page</p>";
+?>
 
+
+<div class="cart-overview">
+
+	<ul class="products">
+
+	<?php foreach ($cart['courses'] as $product => $value) {
+
+	$moodecCourse = $DB->get_record('local_moodec_course', array('courseid' => $product));
+	$thisCourse = get_course($product);?>
+
+		<li class="product-item">
+			<h4 class="product-title"><?php echo $thisCourse->fullname;?></h4>
+			<div class="product-price">$<?php echo $moodecCourse->price;?></div>
+			<form class="product__form" action="" method="POST">
+				<input type="hidden" name="id" value="<?php echo $product;?>">
+				<input type="hidden" name="action" value="removeFromCart">
+				<input class="form__submit" type="submit" value="Remove">
+			</form>
+		</li>
+
+	<?php }?>
+
+	</ul>
+
+	<div class="cart-summary">
+		<h3 class="cart-total__label">Total:</h3><h3 class="cart-total">$<?php echo local_moodec_cart_get_total();?></h3>
+	</div>
+
+	<div class="cart-actions">
+		<button>Return to store</button>
+		<button>Proceed to checkout</button>
+	</div>
+</div>
+
+
+<?php
 echo $OUTPUT->footer();
--- moodec/pages/product.php
+++ moodec/pages/product.php
@@ -18,6 +18,12 @@ if (!($course = $DB->get_record('course', array('id' => $courseid)))) {
 	print_error('invalidcourseid', 'error');
 }
 
+$moodecCourse = $DB->get_record('local_moodec_course', array('courseid' => $courseid));
+
+if (!$moodecCourse->show_in_store) {
+	print_error('courseunavailable', 'error');
+}
+
 $PAGE->set_url('/local/moodec/pages/product.php', array('id' => $courseid));
 $PAGE->set_pagelayout('standard');
 
@@ -25,9 +31,35 @@ echo $OUTPUT->header();
 
 echo $OUTPUT->heading($course->fullname);
 
-echo "<p>Product details:</p>";
-echo "<pre>";
-var_dump($course);
-echo "</pre>";
+?>
+
+<div class="product-single">
+	<img src="<?php echo local_moodec_get_course_image_url($moodecCourse->courseid);?>" alt="" class="product-image">
+	<div class="product-details">
+		<div class="product-description">
+			<?php echo local_moodec_format_course_summary($moodecCourse->courseid);?>
+		</div>
+
+		<h4>Enrolment Duration</h4>
+		<p><?php echo local_moodec_format_enrolment_duration($moodecCourse->enrolment_duration);?></p>
+
+		<h4>Price: <span class="price">$<?php echo $moodecCourse->price;?></span></h4>
+
+		<form action="/local/moodec/pages/cart.php" method="POST" class="product-single__form">
+			<input type="hidden" name="action" value="addToCart">
+			<input type="hidden" name="id" value="<?php echo $moodecCourse->courseid;?>">
+			<input type="submit" value="Add to cart">
+		</form>
+	</div>
+</div>
+
+<?php
+// echo "<p>" . $course->summary . "</p>";
+// echo "<hr>";
+// echo "<h4>Course Duration</h4>";
+// echo "<p>" . $moodecCourse->enrolment_duration . "</p>";
+// echo "<hr>";
+// echo "<h4>Price</h4>";
+// echo "<p>$" . $moodecCourse->price . "</p>";
 
 echo $OUTPUT->footer();
