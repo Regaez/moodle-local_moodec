d3489254863408c1060fc499243b5757207a5460
Thomas Threadgold <tj.threadgold@gmail.com>
2015-05-12 16:15:18 +1200

    Bumped version and fixed @copyright

--- moodec/version.php
+++ moodec/version.php
@@ -5,14 +5,14 @@
  * @package     local
  * @subpackage  local_moodec
  * @author   	Thomas Threadgold
- * @copyright   2015 LearningWords Ltd
+ * @copyright   2015 LearningWorks Ltd
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'local_moodec';
-$plugin->version = 2015050414;
+$plugin->version = 2015051200;
 $plugin->release = '2.8 (Build: 2015012900)';
 $plugin->requires = 2014051200;
 //$plugin->requires = 2014111000;
