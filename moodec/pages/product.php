3dec0581d4d320a6fd5c61a48002c4f6d06d658a
Thomas Threadgold <tj.threadgold@gmail.com>
2015-05-22 10:25:12 +1200

    Added tier selection box on product page and now using get_product function

--- moodec/pages/product.php
+++ moodec/pages/product.php
@@ -24,18 +24,18 @@ if (!($course = $DB->get_record('course', array('id' => $courseid)))) {
 	print_error('invalidcourseid', 'error');
 }
 
-$moodecCourse = $DB->get_record('local_moodec_course', array('courseid' => $courseid));
-$course = get_course($courseid);
+// $product = $DB->get_record('local_moodec_course', array('courseid' => $courseid));
+$product = local_moodec_get_product($courseid);
 
-if (!$moodecCourse->show_in_store) {
+if (!$product) {
 	print_error('courseunavailable', 'error');
 }
 
 echo $OUTPUT->header();
 
-echo $OUTPUT->heading(get_string('product_title', 'local_moodec', array('coursename' => $course->fullname)));
+echo $OUTPUT->heading(get_string('product_title', 'local_moodec', array('coursename' => $product->fullname)));
 
-$imageURL = local_moodec_get_course_image_url($moodecCourse->courseid);
+$imageURL = local_moodec_get_course_image_url($product->courseid);
 ?>
 
 <div class="product-single">
@@ -48,31 +48,31 @@ $imageURL = local_moodec_get_course_image_url($moodecCourse->courseid);
 
 		<?php if (!!get_config('local_moodec', 'page_product_show_description')) {?>
 		<div class="product-description">
-			<?php echo local_moodec_format_course_summary($moodecCourse->courseid);?>
+			<?php echo local_moodec_format_course_summary($product->courseid);?>
 		</div>
 		<?php }?>
 
 		<?php if (!!get_config('local_moodec', 'page_product_show_additional_description')) {?>
 		<div class="additional-info">
-			<?php echo $moodecCourse->additional_info;?>
+			<?php echo $product->additional_info;?>
 		</div>
 		<?php }?>
 
 		<?php if (!!get_config('local_moodec', 'page_catalogue_show_category')) {
 
-		$category = $DB->get_record('course_categories', array('id' => $course->category));
-		$categoryURL = new moodle_url($CFG->wwwroot . '/local/moodec/pages/catalogue.php', array('category' => $course->category));
+		$category = $DB->get_record('course_categories', array('id' => $product->category));
+		$categoryURL = new moodle_url($CFG->wwwroot . '/local/moodec/pages/catalogue.php', array('category' => $product->category));
 		?>
 		<p><?php echo get_string('course_list_category_label', 'local_moodec');?> <a href="<?php echo $categoryURL;?>"><?php echo $category->name;?></a></p>
 		<?php }?>
 
 		<h4><?php echo get_string('enrolment_duration_label', 'local_moodec');?></h4>
-		<p><?php echo local_moodec_format_enrolment_duration($moodecCourse->enrolment_duration);?></p>
+		<p><?php echo local_moodec_format_enrolment_duration($product->enrolment_duration);?></p>
 
-		<h4><?php echo get_string('price_label', 'local_moodec');?> <span class="price"><?php echo local_moodec_get_currency_symbol(get_config('local_moodec', 'currency')) . $moodecCourse->price;?></span></h4>
+		<h4><?php echo get_string('price_label', 'local_moodec');?> <span class="price"><?php echo local_moodec_get_currency_symbol(get_config('local_moodec', 'currency')) . $product->price;?></span></h4>
 
 		<?php
-if (isloggedin() && is_enrolled(context_course::instance($moodecCourse->courseid, MUST_EXIST))) {
+if (isloggedin() && is_enrolled(context_course::instance($product->courseid, MUST_EXIST))) {
 	?>
 			<div class="product-form">
 				<button class="product-form__add" disabled="disabled"><?php echo get_string('button_enrolled_label', 'local_moodec');?></button>
@@ -80,11 +80,34 @@ if (isloggedin() && is_enrolled(context_course::instance($moodecCourse->courseid
 
 		<?php } else {?>
 
-		<form action="/local/moodec/pages/cart.php" method="POST" class="product-single__form">
-			<input type="hidden" name="action" value="addToCart">
-			<input type="hidden" name="id" value="<?php echo $moodecCourse->courseid;?>">
-			<input type="submit" value="<?php echo get_string('button_add_label', 'local_moodec');?>">
-		</form>
+			<?php if($product->pricing_model === 'simple') { ?>
+
+				<form action="/local/moodec/pages/cart.php" method="POST" class="product-single__form">
+					<input type="hidden" name="action" value="addToCart">
+					<input type="hidden" name="id" value="<?php echo $product->courseid;?>">
+					<input type="submit" value="<?php echo get_string('button_add_label', 'local_moodec');?>">
+				</form>
+
+			<?php } else { ?>
+
+				<form action="/local/moodec/pages/cart.php" method="POST" class="product-single__form">
+					<input type="hidden" name="action" value="addVariationToCart">
+					<select name="variation">
+						
+						<?php foreach($product->variations as $variation) {?>
+						
+							<option value="<?php echo $variation->variation_id; ?>">
+								<?php echo $variation->name; ?>
+							</option>
+						
+						<?php } ?>
+
+					</select>
+					<input type="hidden" name="id" value="<?php echo $product->courseid;?>">
+					<input type="submit" value="<?php echo get_string('button_add_label', 'local_moodec');?>">
+				</form>
+			
+			<?php } ?>
 
 		<?php }?>
 	</div>
@@ -93,7 +116,7 @@ if (isloggedin() && is_enrolled(context_course::instance($moodecCourse->courseid
 <?php
 if (!!get_config('local_moodec', 'page_product_show_related_products')) {
 
-	$products = local_moodec_get_related_products($courseid, $course->category);
+	$products = local_moodec_get_related_products($courseid, $product->category);
 	$iterator = 0;
 	if (is_array($products) && 0 < count($products)) {?>
 
