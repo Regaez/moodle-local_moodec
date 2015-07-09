43091a13dc44d27e10680338c7d90333a658f075
Thomas Threadgold <tj.threadgold@gmail.com>
2015-07-10 10:08:49 +1200

    Added renderer shell

---
+++ renderer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Moodec Renderer
+ *
+ * @package    local_moodec
+ * @copyright  2015 Thomas Threadgold <tj.threadgold@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Moodec Renderer
+ *
+ * @copyright  2015 Thomas Threadgold <tj.threadgold@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class local_moodec_renderer extends plugin_renderer_base {
+
+
+
+}
