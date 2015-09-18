394e4b2bbb15f2ac7760d440f1a3bd5c12b66414
Thomas Threadgold <tj.threadgold@gmail.com>
2015-09-18 15:48:13 +1200

    Lib file now loads the DPS gateway class

--- lib.php
+++ lib.php
@@ -30,6 +30,7 @@ require_once $CFG->dirroot . '/local/moodec/classes/transaction.php';
 require_once $CFG->dirroot . '/local/moodec/classes/transaction_item.php';
 require_once $CFG->dirroot . '/local/moodec/classes/gateway.php';
 require_once $CFG->dirroot . '/local/moodec/classes/gateway_paypal.php';
+require_once $CFG->dirroot . '/local/moodec/classes/gateway_dps.php';
 
 /**
  * Extend the default Moodle navigation
