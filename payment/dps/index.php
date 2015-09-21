0923144e01b9acb00ccd7f57fadd5ff7b0e40b62
Thomas Threadgold <tj.threadgold@gmail.com>
2015-09-21 14:00:16 +1200

    Removed unnecessary code in the dps gateway page

--- payment/dps/index.php
+++ payment/dps/index.php
@@ -12,26 +12,13 @@
 require_once dirname(__FILE__) . '/../../../../config.php';
 require_once $CFG->dirroot . '/local/moodec/lib.php';
 
-// var_dump($_POST['id']);
+$transactionID = required_param('id', PARAM_INT);  // plugin instance id
 
-// $transactionID = required_param('id', PARAM_INT);  // plugin instance id
-$transactionID = (int) $_POST['id'];
-// Set PAGE variables
-// $PAGE->set_context(context_system::instance());
-// $PAGE->set_url(new moodle_url('/local/moodec/payment/dps/', array('id' => $transactionID)));
-
-//require_login();
+require_login();
 
 $gateway = new MoodecGatewayDPS($transactionID);
 
-// echo "<pre>";
-
 $response = $gateway->begin();
-
-// var_dump($response);
-
-// echo "</pre>";
-// exit;
 
 // abort if DPS returns an invalid response
 if ($response->attributes()->valid != '1') {
