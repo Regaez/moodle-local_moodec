daf3e8be0f9b40f59211ea967fa8721f58f55834
Thomas Threadgold <tj.threadgold@gmail.com>
2015-09-25 11:51:50 +1200

    Removed old upgrade script no longer needed

--- db/upgrade.php
+++ db/upgrade.php
@@ -4,20 +4,7 @@ function xmldb_local_moodec_upgrade($oldversion) {
     global $DB;
     $dbman = $DB->get_manager();
 
-    if ($oldversion < 2015080400) {
-
-        // Define field product_tags to be added to local_moodec_course.
-        $table = new xmldb_table('local_moodec_course');
-        $field = new xmldb_field('product_tags', XMLDB_TYPE_TEXT, null, null, null, null, null, 'additional_info');
-
-        // Conditionally launch add field product_tags.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Moodec savepoint reached.
-        upgrade_plugin_savepoint(true, 2015080400, 'local', 'moodec');
-    }
+    // WILL INSERT UPGRADE STUFF HERE
 
     return true;
 }
